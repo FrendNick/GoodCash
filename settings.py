491e0f5640f5f19cdb0ab1fc94a35e828307937c
FrendNick <96546491+FrendNick@users.noreply.github.com>
2022-01-04 13:47:35 -0600

    add a line

--- settings.py
+++ settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jjwf3nq8v5edvm-ruoq-n*dw$d63pehk4(zc4-729#(*0uqrvp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    ' Data', 
     ' Good ',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
 ]
 
 MIDDLEWARE = [
--- urls.py
+++ urls.py
@@ -14,13 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include , path
+from django.urls import  path
 from django.http import httpResponse 
 
-
+def note ( request ) :
+    return httpResponse( " welcome here ")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Good/', include( " Good.urls") )
+    path ('', note ),
+    
 ]
+ 
  
